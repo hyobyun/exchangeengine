dd4d913f12acbcbdd5df9505a7630162afc3c312
Hyo Byun <hyo.yul.byun@gmail.com>
2015-03-04 00:42:39 -0500

    organized, comments

--- modules/orderBook/Book.js
+++ modules/orderBook/Book.js
@@ -1,9 +1,10 @@
 // Exchange Engine
 // Hyo Yul Byun 2015
+
 var BinaryHeap = require('./BinaryHeap.js');
 
 function Book() {
-	this.bids	= new BinaryHeap();
-	this.asks	= new BinaryHeap();
+	this.bids = new BinaryHeap();
+	this.asks = new BinaryHeap();
 }
 module.exports = Book;
--- modules/orderBook/Order.js
+++ modules/orderBook/Order.js
@@ -2,10 +2,12 @@
 // Hyo Yul Byun 2015
 
 function Order() {
-	this.ownerID	=null;
-	this.assetID	=null;
-	this.type		=null;
-	this.price		=null;
-	this.quantity	=null;
+	this.ownerID = null;
+	this.assetID = null;
+	this.time = null;
+	this.orderType = null;
+	this.price = null;
+	this.quantity = null;
+	this.status = null;
 }
 module.exports = Order;
