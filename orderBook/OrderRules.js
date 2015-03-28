7f886e66181cefa377f59acf6f063de7edff3bcf
Hyo Byun <hyo.yul.byun@gmail.com>
2015-03-28 09:43:01 -0400

    Order Rules

---
+++ orderBook/OrderRules.js
@@ -0,0 +1,19 @@
+// Exchange Engine
+// Hyo Yul Byun 2015
+
+// ask sellers asking for
+
+// Bid - buyers bidding
+
+
+module.exports = {
+  limit : function(agroOrder, restOrders) {
+    if (agroOrder.side == 'a') {
+
+    } else if (agroOrder.side == 'b') {
+
+    }
+  }
+
+
+}
