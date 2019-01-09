43a91c7e87541984133967b74c771372c05732fb
Hyo Byun <hyobyun@Hyos-MacBook-Pro.local>
2019-01-08 22:40:27 -0500

    removed config.js, updated template

--- config_template.js
+++ config_template.js
@@ -1,6 +1,14 @@
+var MATCHALGOS = require('./constants/matchAlgos.js');
 var config = {};
 
+
+config.sessionSecret='changeme';
+config.defaultMatchAlgo = MATCHALGOS.FIFO;
+// Default matching algorithm used
+config.defaultMatchAlgo = 1; // 1:ProRata
+config.orderTypes = ['limit','market'];
 config.deciAccuracy = 0.01;
 
+config.port = 8888;
 
 module.exports = config;
